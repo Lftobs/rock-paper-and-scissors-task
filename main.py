218dd50dcd9b1aac3dd9174b139b22a239bfdcba
Lftobs <106287048+Lftobs@users.noreply.github.com>
2022-06-03 02:15:54 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,66 @@
+
+#rock paper or scissors game
+
+import random
+print("This is a rock, paper and  scissors game.")
+print("R = rock")
+print("P = paper")
+print("S = scissors")
+while True:
+    choices = ["r","p","s"]
+
+    computer = random.choice(choices)
+    player = None
+
+    while player not in choices:
+        player = input("Enter your choice : ").lower()
+        print(f"\nYou chose {player}, computer chose {computer}.\n")
+
+
+    if player == computer:
+        print(f"Both player selected {player}. Hence it's a tie")
+  #    print("player: ",player)
+     #   print("Tie!")
+
+    elif player == "r":
+        if computer == "p":
+            print("Paper covers rock. You lose!")
+     #       print("computer: ", computer)
+       #     print("player: ", player)
+      #      print("You lose!")
+        else:
+            print("Rock smashes scissors. You win!")
+        #    print("computer: ", computer)
+        #    print("player: ", player)
+        #    print("You win!")
+
+    elif player == "s":
+        if computer == "r":
+            print("Rock smashes scissors. You lose!")
+          #  print("computer: ", computer)
+       #     print("player: ", player)
+      #      print("You lose!")
+        else:
+            print("Scissors cuts paper. You win!")
+         #   print("computer: ", computer)
+          #  print("player: ", player)
+          #  print("You win!")
+
+    elif player == "p":
+        if computer == "s":
+            print("Scissors cuts paper. You lose!")
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You lose!")
+        else:
+            print("Paper covers rock. You win!")
+        #    print("computer: ", computer)
+        #    print("player: ", player)
+         #   print("You win!")
+
+    play_again = input("Play again? (yes/no): ").lower()
+
+    if play_again != "yes":
+        break
+
+print("Bye!")
